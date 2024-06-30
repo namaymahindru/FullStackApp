23620c03693fdc0533c134ca77707a48c4952733
namaymahindru <namaym9@gmail.com>
2024-06-30 20:50:55 +0530

    changes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+export const test = (req, res) => {
+    
+    res.json({
+
+        messgae: 'Api Routes',
+
+    })
+}
---
+++ api/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+//console.log("MongoDB connection string:", process.env.MONGO);
+
+app.use(express.json());// this allows to send json data to the server
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Start the server only after a successful connection to MongoDB
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000!");
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json(
+    {
+      success: false,
+      statusCode,
+      message,
+
+    }
+  )
+
+})
