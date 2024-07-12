7d369afa84324f364bcbe4f1d3a71d8ed7ced829
namaymahindru <namaym9@gmail.com>
2024-07-12 16:55:11 +0530

    added more OAuth Components

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,7 +15,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
    
-  }
+  },
+  avatar: {
+    type: String,
+    default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fuser-profile.html&psig=AOvVaw04HjW935HGqg_SFSB3cq_w&ust=1720869564619000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPDEiOywoYcDFQAAAAAdAAAAABAE"
+  },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { GoogleAuthProvider,getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 export default function OAuth() {
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
     
         try {
@@ -10,15 +15,31 @@ export default function OAuth() {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app)
 
-            const result = await signInWithPopup(auth,provider)
-            console.log(result);
-        } catch(error) {
+            const result = await signInWithPopup(auth, provider);
+            // console.log(result);
             
-            console.log('Could not sign with Google', error); 
+            const res = await fetch('/api/auth/google', {
+                
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name: result.user.displayName, email: result.user.email, photo: result.user.photoURL }),
+                
+
+            })
+
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+            navigate('/');
+
+        } catch (error) {
+            
+            console.log('Could not sign with Google', error);
 
         }
 
-}
+    };
 
   return (
     <button onClick={handleGoogleClick} type='button'className='bg-red-800 text-white p-3 rounded-lg uppercase hover: opacity-95'>Continue with Google</button>
